28c347b5542ca93d0888947d2bab6774fd0d4306
Qiusheng Wu <giswqs@gmail.com>
2018-05-14 13:17:47 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 """The setup script."""
-
+from os import path as op
+import io
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
@@ -10,6 +11,15 @@ with open('README.rst') as readme_file:
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
+
+here = op.abspath(op.dirname(__file__))
+
+# get the dependencies and installs
+with io.open(op.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
 
 requirements = ['Click>=6.0', ]
 
@@ -38,7 +48,8 @@ setup(
             'lidar=lidar.cli:main',
         ],
     },
-    install_requires=requirements,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
