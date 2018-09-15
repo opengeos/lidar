21a918f355c202bd29c37731ff6a8df11604c6aa
Qiusheng Wu <giswqs@gmail.com>
2018-09-15 19:41:07 -0400

    Bump version: 0.1.9 → 0.1.10

--- lidar/__init__.py
+++ lidar/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Qiusheng Wu"""
 __email__ = 'giswqs@gmail.com'
-__version__ = '0.1.9'
+__version__ = '0.1.10'
 
 from .filling import ExtractSinks
 from .slicing import DelineateDepressions
--- setup.py
+++ setup.py
@@ -79,6 +79,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giswqs/lidar',
-    version='0.1.9',
+    version='0.1.10',
     zip_safe=False,
 )
