262ea8e2052049f7a73c118ead4bbb6021c3d8d2
Qiusheng Wu <giswqs@gmail.com>
2019-08-15 21:14:45 -0400

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -87,6 +87,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giswqs/lidar',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
