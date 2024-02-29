b84145af1d7858889594b0bfab98bff6c512f6fc
Qiusheng Wu <giswqs@gmail.com>
2024-02-29 09:17:59 -0500

    Bump version: 0.7.4 → 0.8.0

--- lidar/__init__.py
+++ lidar/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Qiusheng Wu"""
 __email__ = "giswqs@gmail.com"
-__version__ = "0.7.4"
+__version__ = "0.8.0"
 
 from .filling import (
     ExtractSinks,
--- setup.py
+++ setup.py
@@ -114,6 +114,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giswqs/lidar",
-    version="0.7.4",
+    version="0.8.0",
     zip_safe=False,
 )
