8ea7e96355f0d862c1d892335d00743093564a0f
Qiusheng Wu <giswqs@gmail.com>
2025-01-30 00:15:28 -0500

    Bump version: 0.8.3 → 0.8.4

--- setup.py
+++ setup.py
@@ -114,6 +114,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/opengeos/lidar",
-    version="0.8.3",
+    version="0.8.4",
     zip_safe=False,
 )
